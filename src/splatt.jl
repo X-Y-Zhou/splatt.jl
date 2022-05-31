cde33ccc92a0713816da98fd2ab1e02fe0a4447a
X-Y-Zhou <2827644213@qq.com>
2022-05-31 20:27:15 +0800

    update function

--- src/splatt.jl
+++ src/splatt.jl
@@ -2,7 +2,6 @@ module splatt
 
 # Write your package code here.
 using Random,NLsolve,Distributions
-import Random:seed!
 function add_number(a,b)
     a+b
 end
@@ -17,7 +16,7 @@ function lognormal_stats(x)
     return [Mean, Var]
 end
 
-function lognormal_perturb(x; percentage = 0.1, initial_x = [0.0,10.0], rng = Random.seed!())
+function lognormal_perturb(x; percentage = 0.1, initial_x = [0.0,10.0])
     if x == 0
         y = 0
     else
@@ -26,7 +25,7 @@ function lognormal_perturb(x; percentage = 0.1, initial_x = [0.0,10.0], rng = Ra
         residual_lognormal(x) = lognormal_stats(x) .- [Ex,Dx]
         solution = NLsolve.nlsolve(residual_lognormal, initial_x)
         a, b = solution.zero
-        y = rand(rng, LogNormal(a, b))
+        y = rand(LogNormal(a, b))
     end
     return Float64(y)
 end
--- test/testfunction.jl
+++ test/testfunction.jl
@@ -7,3 +7,4 @@ splatt.add_number(3,2)
 splatt.add_three_number(2,2,3)
 
 splatt.lognormal_perturb(2; percentage = 0.1)
+
