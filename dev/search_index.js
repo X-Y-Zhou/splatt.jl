07125d0eb4afa3d2af3b28a4e0a9a535534325d3
Documenter.jl <documenter@juliadocs.github.io>
2022-05-31 07:26:53 +0000

    build based on 71b1802

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = splatt","category":"page"},{"location":"#splatt","page":"Home","title":"splatt","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for splatt.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [splatt]","category":"page"}]
+}
