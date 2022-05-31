d895f15a9621d7716f8afd97fb3d70a0fb034845
X-Y-Zhou <2827644213@qq.com>
2022-05-31 16:14:13 +0800

    add test

--- src/splatt.jl
+++ src/splatt.jl
@@ -6,4 +6,8 @@ function add_number(a,b)
     a+b
 end
 
+function add_three_number(a,b,c)
+    a+b+c
+end
+
 end
---
+++ test/testfunction.jl
@@ -0,0 +1,8 @@
+using splatt
+using NLsolve
+
+splatt.add_number(1,2)
+splatt.add_number(3,2)
+splatt.add_three_number(2,2,3)
+
+splatt.lognormal_perturb1(2; percentage = 0.1)
