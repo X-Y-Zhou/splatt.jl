2c37ef39584b17b753db6a2b709f2898c09f6abd
X-Y-Zhou <2827644213@qq.com>
2022-05-31 16:15:32 +0800

    Update splatt.jl

--- src/splatt.jl
+++ src/splatt.jl
@@ -10,4 +10,18 @@ function add_three_number(a,b,c)
     a+b+c
 end
 
+function lognormal_perturb1(x; percentage = 0.1, initial_x = [0.0,10.0], rng = Random.seed!())
+    if x == 0
+        y = 0
+    else
+        Ex = x
+        Dx = (percentage*x)^2
+        residual_lognormal(x) = lognormal_stats(x) .- [Ex,Dx]
+        solution = NLsolve.nlsolve(residual_lognormal, initial_x)
+        a, b = solution.zero
+        y = rand(rng, LogNormal(a, b))
+    end
+    return Float64(y)
+end
+
 end
